476cb3981e05aeb33b0b04d065ab45a0c22c58d4
Tõnis Simo <anton.estum@gmail.com>
2013-04-07 02:07:34 +0400

    Fix

--- lib/growlyflash/controller.rb
+++ lib/growlyflash/controller.rb
@@ -8,7 +8,7 @@ module Growlyflash
       after_filter :flash_to_headers, if: :is_xhr_request?
       
       def flash_to_headers
-        flash_json = raw(Hash[ flash.map { |type, msg| [type, msg] } ].to_json)
+        flash_json = Hash[ flash.map { |type, msg| [type, msg] } ].to_json
         response.headers['X-Message'] = flash_json.clone
         flash.discard # don't want the flash to appear when you reload page
       end
--- lib/growlyflash/version.rb
+++ lib/growlyflash/version.rb
@@ -1,3 +1,3 @@
 module Growlyflash
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
